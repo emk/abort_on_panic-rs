cb56e414955549477fb7b90c1bdcf5e1498324e2
Eric Kidd <git@randomhacks.net>
2015-05-10 12:29:39 -0400

    v0.0.8: Remove use of unstable features

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,13 @@
 //! }
 //! ```
 
-#![feature(core)]
-
-use std::intrinsics::abort;
 use std::io::{stderr, Write};
 use std::thread::panicking;
+
+extern {
+    // OS-level abort function, because std::intrinsics::abort is unstable.
+    fn abort();
+}
 
 /// Once this object is created, it can only be destroyed in an orderly
 /// fashion.  Attempting to clean it up from a panic handler will abort the
